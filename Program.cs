9a6eae8565652ba0027fe97c00c872b84342e78f
Kahu Arona-Haua (1000121223) <ARANK2@student.op.ac.nz>
2024-09-26 15:29:47 +1200

    new code

--- Program.cs
+++ Program.cs
@@ -6,6 +6,10 @@
         {
             Console.WriteLine("My name is Kahu Arona-Haua");
         }
-        static void Main(string[] args) { }
+        static void Main(string[] args)
+        {
+            Kahu();
+            Console.ReadLine(); 
+        }
     }
 }
