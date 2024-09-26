bdf3d933339edccbbce2be7d654495606dee25e9
Manish Singh (1000129514) <SINGM66@student.op.ac.nz>
2024-09-26 15:31:51 +1200

    Program.cs

--- Program.cs
+++ Program.cs
@@ -1,10 +1,16 @@
-﻿namespace Console_Project
+﻿using System;
+
+namespace Console_Project
 {
     internal class Program
     {
         public static void Kahu()
         {
             Console.WriteLine("My name is Kahu Arona-Haua");
+        }
+        public static void Manihs()
+        {
+            Console.WriteLine("Manish here!!!");
         }
         static void Main(string[] args) { }
     }
